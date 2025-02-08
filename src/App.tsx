34682d214e1aa2b1c4eebe58aa9fd4982be174e8
ryu <114303361+ryuapp@users.noreply.github.com>
2025-02-08 19:50:30 +0900

    feat: add editor example

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import Editor from "./editor.tsx";
 import "./global.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const focusEditor = () => {
+    const editor = document.querySelector(".tiptap") as HTMLElement;
+    editor.focus();
+    const range = document.createRange();
+    range.selectNodeContents(editor);
+    range.collapse(false);
+    const sel = globalThis.window.getSelection()!;
+    sel.removeAllRanges();
+    sel.addRange(range);
+  };
 
   return (
-    <main className="mx-2">
-      <h1>Welcome to Jadeite</h1>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <div className="flex gap-2">
-          <input
-            id="greet-input"
-            className="border p-1"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button className="border p-1 hover:cursor-pointer" type="submit">
-            Greet
-          </button>
-        </div>
-      </form>
-      <p>{greetMsg}</p>
+    <main className="h-screen flex flex-col">
+      <Editor />
+      <div className="flex-1 hover:cursor-text" onClick={() => focusEditor()}>
+      </div>
     </main>
   );
 }
