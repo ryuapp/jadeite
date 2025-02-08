1fe2cff450c64005b6c1398f2664f75684bc5654
ryu <114303361+ryuapp@users.noreply.github.com>
2025-02-09 00:42:54 +0900

    fix: expand editor to fill screen

--- src/app.tsx
+++ src/app.tsx
@@ -2,22 +2,9 @@ import Editor from "./editor.tsx";
 import "./global.css";
 
 function App() {
-  const focusEditor = () => {
-    const editor = document.querySelector(".tiptap") as HTMLElement;
-    editor.focus();
-    const range = document.createRange();
-    range.selectNodeContents(editor);
-    range.collapse(false);
-    const sel = globalThis.window.getSelection()!;
-    sel.removeAllRanges();
-    sel.addRange(range);
-  };
-
   return (
-    <main className="h-screen flex flex-col">
+    <main className="flex flex-col">
       <Editor />
-      <div className="flex-1 hover:cursor-text" onClick={() => focusEditor()}>
-      </div>
     </main>
   );
 }
